d5b0df082e28c2f79ae334620a17624abd2283ca
orang3world <aaorange75@gmail.com>
2024-03-26 20:38:02 -0300

    acceso admin y modelos Proveedor y Producto visibles

--- StockControl/compra/admin.py
+++ StockControl/compra/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Producto, Proveedor
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'precio','stock_actual', 'proveedor_id']
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido','dni']
+
+
 # Register your models here.
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Proveedor, ProveedorAdmin)
