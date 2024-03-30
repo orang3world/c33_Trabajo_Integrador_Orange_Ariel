f5465b1dcb7c135f825ea23575972c06b5dd01da
orang3world <aaorange75@gmail.com>
2024-03-29 21:39:40 -0300

    form para productos ok. Nota: usando ModelForm

--- StockControl/compra/forms.py
+++ StockControl/compra/forms.py
@@ -3,19 +3,19 @@ from django.forms import ModelForm
 from .models import Proveedor, Producto
 
 class ProductoForm(ModelForm):
-    nombre = forms.CharField()
+    '''nombre = forms.CharField()
     precio = forms.FloatField()
     stock_actual = forms.IntegerField()
     proveedor = forms.ChoiceField()
-    #proveedor = forms.ChoiceField(
+    #proveedor = forms.ChoiceField('''
     class Meta:
         model = Producto
         fields = ['nombre', 'precio', 'stock_actual','proveedor']
     
 class ProveedorForm(ModelForm):
-    nombre = forms.CharField()
+    '''nombre = forms.CharField()
     apellido = forms.CharField()
-    dni = forms.IntegerField()
+    dni = forms.IntegerField()'''
     class Meta:
         model = Proveedor
         fields = ['nombre', 'apellido', 'dni']
