0ac994e20731ad4d7549486dbd3e6e6874712fbc
orang3world <aaorange75@gmail.com>
2024-03-30 19:27:29 -0300

    uso de modelForm para formularios con bootstrap sin usar app, barra de navegacion agregada

--- StockControl/compra/forms.py
+++ StockControl/compra/forms.py
@@ -1,3 +1,5 @@
+import attrs
+from django import forms
 from django.forms import ModelForm
 from .models import Proveedor, Producto
 
@@ -6,7 +8,19 @@ class ProductoForm(ModelForm):
         model = Producto
         fields = ['nombre', 'precio', 'stock_actual','proveedor']
     
+        widgets = {
+            'nombre':forms.TextInput(attrs={'class':'form-control'}),
+            'precio':forms.TextInput(attrs={'class':'form-control'}),
+            'stock_actual':forms.TextInput(attrs={'class':'form-control'}),
+            'proveedor':forms.Select(attrs={'class':'form-select'})
+        }
 class ProveedorForm(ModelForm):
     class Meta:
         model = Proveedor
         fields = ['nombre', 'apellido', 'dni']
+        
+        widgets = {
+            'nombre':forms.TextInput(attrs={'class':'form-control'}),
+            'apellido':forms.TextInput(attrs={'class':'form-control'}),
+            'dni':forms.TextInput(attrs={'class':'form-control'})
+        }
