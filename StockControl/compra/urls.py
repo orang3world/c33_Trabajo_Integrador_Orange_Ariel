12c87c27459ed8d17d63c6ced74b70f79ae0dcf7
orang3world <aaorange75@gmail.com>
2024-04-09 10:56:17 -0300

    sidebar create ok

--- StockControl/compra/urls.py
+++ StockControl/compra/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('crear/<str:seccion>/', views.crear, name='crear'),
-    path("listar/<str:seccion>/", views.listar, name='listar')
-    ]
+    path("", views.inicio, name="inicio"),
+    path("crear_productos/", views.crear_productos, name="crear_productos"),
+    path("crear_proveedores/", views.crear_proveedores, name="crear_proveedores"),
+    path("productos/", views.productos, name="productos"),
+    path("proveedores/", views.proveedores, name="proveedores"),
+]
--- StockControl/compra/views.py
+++ StockControl/compra/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
-
 from .models import Producto, Proveedor
 from .forms import ProductoForm, ProveedorForm
 
@@ -20,56 +19,59 @@ C R U D
 '───────X──> Create ──> proveedor
                     ──> producto
 """
+context = {
+    "navbar": "navbar.html",
+    "sidebar": "sidebar.html",
+    "content": "content.html",
+    "subcontent": "index.html",
+    "footer": "footer.html",
+    "base": "base.html",
+}
+
+
 def inicio(request):
-    return render(request,'base.html')
+    return render(request, "base.html", context)
 
 
-def crear(request, seccion):
-    if seccion == "productos":
+def crear_productos(request):
         if request.method == "POST":
             form = ProductoForm(request.POST)
             if form.is_valid():
                 form.save()
-            return listar(request, seccion)
+            return productos(request)
         else:
             form = ProductoForm()
-        return render(request, "prod_form.html", {"form": ProductoForm})
-    elif seccion == "proveedores":
-        if request.method == "POST":
-            form = ProveedorForm(request.POST)
-            if form.is_valid():
-                form.save()
-            return listar(request, seccion)
-        else:
-            form = ProveedorForm()
-        return render(request, "prov_form.html", {"form": ProveedorForm})
+        context["form"] = ProductoForm
+        context["subcontent"] = "prod_form.html"
+
+        return render(request, "prod_form.html", context)
+    
+def crear_proveedores(request):
+    if request.method == "POST":
+        form = ProveedorForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return proveedores(request)
+    else:
+        form = ProveedorForm()
+    context["form"] = ProveedorForm
+    context["subcontent"] = "prov_form.html"
+    return render(request, "prov_form.html", context)
 
 
-def listar(request, seccion):
-    if seccion == "productos":
-        productos = Producto.objects.all()
-        return render(request, "listar_productos.html", {"productos": productos})
-    elif seccion == "proveedores":
-        proveedores = Proveedor.objects.all()
-        return render(request, "listar_proveedores.html", {"proveedores": proveedores})
-    
-    
-def actualizar(request, seccion):
-    if seccion == "productos":
-        if request.method == "POST":
-            form = ProductoForm(request.POST)
-            if form.is_valid():
-                form.save()
-            return listar(request, seccion)
-        else:
-            form = ProductoForm()
-        return render(request, "prod_form.html", {"form": ProductoForm})
-    elif seccion == "proveedores":
-        if request.method == "POST":
-            form = ProveedorForm(request.POST)
-            if form.is_valid():
-                form.save()
-            return listar(request, seccion)
-        else:
-            form = ProveedorForm()
-        return render(request, "prov_form.html", {"form": ProveedorForm})
+def productos(request):
+    link_crear='/crear_productos/'
+    productos = Producto.objects.all()
+    context["productos"] = productos
+    context["subcontent"] = "productos.html"
+    context["link_crear"] = link_crear
+    return render(request, "productos.html", context)
+
+
+def proveedores(request):
+    link_crear = "/crear_proveedores/"
+    proveedores = Proveedor.objects.all()
+    context["proveedores"] = proveedores
+    context["subcontent"] = "proveedores.html"
+    context["link_crear"] = link_crear
+    return render(request, "proveedores.html", context)
